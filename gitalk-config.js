6865d841b5c9d6e24470f6c4e5f4fca7d20e09b0
luoweiWHUT <1615108374@qq.com>
2024-05-21 15:14:19 +0800

    update at 2024/05/21 周二 15:14:18.10

--- gitalk-config.js
+++ gitalk-config.js
@@ -1,7 +1,7 @@
 // str += '\n\n<link rel="stylesheet" href="https://unpkg.com/gitalk/dist/gitalk.css">' +
 //     '\n\n<script src="https://unpkg.com/gitalk@latest/dist/gitalk.min.js"></script>' +
 //     '\n\n<div id="gitalk-container"></div>' +
-//     '\n\n<script src="https://luowei01.github.io/blogs/gitalk-config.js"></script>';手动加在某个插件中运行
+//     '\n\n<script src="https://luowei01.github.io/blogs/gitalk-config.js"></script>';手动加在某个插件的inndex.js中运行
 var gitalk = new Gitalk({
     "clientID": "Ov23liXJ47TD4diLy6Z5",
     "clientSecret": "00f665c53d4f4f4ef1a0c8b86a918636233b54c5",
